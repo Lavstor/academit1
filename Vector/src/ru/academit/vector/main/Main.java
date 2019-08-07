16a7b6324c6c1845128dd4cfb823864de4fb9257
Lavstor <45934398+Lavstor@users.noreply.github.com>
2019-08-07 13:33:08 +0300

    vector 2

--- Vector/src/ru/academit/vector/main/Main.java
+++ Vector/src/ru/academit/vector/main/Main.java
@@ -4,13 +4,89 @@ import ru.academit.vector.vectors.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(5);
-        System.out.println(vector);
-        double[] array = {2,3,4,5,6,7};
-        double[] array2 = {2,3,4};
-        vector.fillArray(array);
-        vector.fillArray(array2);
-        System.out.println(vector);
-        System.out.println(vector.getVectorCopy());
+        Vector vector1 = new Vector(2);
+        Vector vector2 = new Vector(5);
+        Vector vector3 = new Vector(2);
+
+        double[] array = {2, 3, 4, 5, 6, 7};
+
+        double[] array2 = {2, 0, 4};
+        double[] array3 = {-20, 5};
+
+
+        vector1.fillArray(array);
+        vector2.fillArray(array);
+        vector3.fillArray(array3);
+
+        System.out.println(vector1);
+        System.out.println(vector2);
+        vector1.getDifference(vector2);
+        System.out.println("Разность "+ vector1);
+        System.out.println();
+
+        System.out.println(vector1);
+        System.out.println(vector2);
+        vector1.getSum(vector2);
+        System.out.println("Сумма "+ vector1);
+        System.out.println();
+
+        System.out.println(vector1);
+        System.out.println(vector2);
+        vector1.getSum(vector2);
+        System.out.println("Сумма "+ vector1);
+        System.out.println();
+
+        Vector vector4 = vector3.getVectorCopy();
+
+        System.out.println(vector3);
+        System.out.println("Копия "+ vector4);
+        System.out.println();
+
+        System.out.println(vector4);
+        vector4.getMultiplication(10);
+        System.out.println("Умножение "+ vector4);
+        System.out.println();
+
+        System.out.println(vector4);
+        vector4.getUturn();
+        System.out.println("Разворот "+ vector4);
+        System.out.println();
+
+        System.out.println(vector4);
+        System.out.println("Размер "+ vector4.getLength());
+        System.out.println();
+
+        System.out.println(vector4);
+        vector4.setArgument(0,0);
+        System.out.println("Поменяли " + vector4);
+        System.out.println("Аргумент " + vector4.getArgument(0));
+        System.out.println();
+
+        System.out.println(vector4);
+        vector4.setArgument(0,0);
+        System.out.println("Поменяли " + vector4);
+        System.out.println("Аргумент " + vector4.getArgument(0));
+        System.out.println();
+
+        System.out.println(vector1);
+
+        Vector vector6 = new Vector(3);
+        vector6.fillArray(array2);
+        System.out.println(vector6);
+
+        Vector vector5 = Vector.getVectorComposition(vector1,vector6);
+        System.out.println(vector5);
+        System.out.println();
+
+
+
+        System.out.println(vector1);
+        System.out.println(vector3);
+
+
+        Vector vector7 = Vector.getVectorDifference(vector1,vector3);
+        System.out.println(vector7);
+
+        Vector vector8 = new Vector(0);
     }
 }
--- Vector/src/ru/academit/vector/vectors/Vector.java
+++ Vector/src/ru/academit/vector/vectors/Vector.java
@@ -1,12 +1,16 @@
 package ru.academit.vector.vectors;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Vector {
     private int n;
     private double[] array;
 
     public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Размерность должна быть больше 0");
+        }
         this.n = n;
         array = new double[n];
     }
@@ -26,7 +30,7 @@ public class Vector {
 
     public void fillArray(double[] donor) {
         if (donor.length < this.n) {
-            Arrays.fill(this.array, this.n, array.length, 0.0);
+            Arrays.fill(this.array, donor.length, this.n, 0.0);
         }
         for (int i = 0; i < donor.length; i++) {
             this.array[i] = donor[i];
@@ -34,5 +38,145 @@ public class Vector {
                 break;
             }
         }
+    }
+
+    public void getSum(Vector vector) {
+        if (vector.n > this.n) {
+            double[] arrayCopy = this.array;
+            this.n = vector.n;
+            this.array = new double[vector.n];
+
+            fillArray(arrayCopy);
+        }
+        for (int i = 0; i < vector.n; i++) {
+            this.array[i] = this.array[i] + vector.array[i];
+        }
+    }
+
+    public void getDifference(Vector vector) {
+        if (vector.n > this.n) {
+            double[] arrayCopy = this.array;
+
+            this.n = vector.n;
+
+            this.array = new double[vector.n];
+
+            fillArray(arrayCopy);
+        }
+        for (int i = 0; i < vector.n; i++) {
+            this.array[i] = this.array[i] - vector.array[i];
+        }
+    }
+
+    public void getMultiplication(int number) {
+        for (int i = 0; i < this.n; i++) {
+            this.array[i] = this.array[i] * number;
+        }
+    }
+
+    public void getUturn() {
+        for (int i = 0; i < this.n; i++) {
+            this.array[i] = this.array[i] * -1;
+        }
+    }
+
+    public double getLength() {
+        double length = 0;
+
+        for (int i = 0; i < this.n; i++) {
+            length += Math.pow(this.array[i], 2);
+        }
+        return Math.sqrt(length);
+    }
+
+    public void setArgument(int index, double num) {
+        if (index >= this.n) {
+            return;
+        }
+        array[index] = num;
+    }
+
+    public double getArgument(int index) {
+        if (index >= this.n) {
+            return 0.0;
+        }
+        return array[index];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vector vector = (Vector) o;
+
+        return n == vector.n && Arrays.equals(array, vector.array);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(n);
+
+        result = 31 * result + Arrays.hashCode(array);
+        return result;
+    }
+
+    public static Vector getVectorSum(Vector vector1, Vector vector2) {
+        int minN = Math.min(vector1.n, vector2.n);
+        int maxN = Math.max(vector1.n, vector2.n);
+
+        Vector vector3 = new Vector(maxN);
+
+        for (int i = 0; i < maxN; i++) {
+            if (i < minN) {
+                vector3.array[i] = vector1.array[i] + vector2.array[i];
+                continue;
+            }
+            if (vector1.array.length > vector2.array.length) {
+                vector3.array[i] = vector1.array[i];
+            } else {
+                vector3.array[i] = vector2.array[i];
+            }
+        }
+        return vector3;
+    }
+
+    public static Vector getVectorDifference(Vector vector1, Vector vector2) {
+
+        int minN = Math.min(vector1.n, vector2.n);
+        int maxN = Math.max(vector1.n, vector2.n);
+
+
+        Vector vector3 = new Vector(maxN);
+
+        for (int i = 0; i < maxN; i++) {
+            if (i < minN) {
+                vector3.array[i] = vector1.array[i] - vector2.array[i];
+                continue;
+            }
+            if (vector1.array.length > vector2.array.length) {
+                vector3.array[i] = vector1.array[i] - 0;
+            } else {
+                vector3.array[i] = -vector2.array[i];
+            }
+        }
+        return vector3;
+    }
+
+    public static Vector getVectorComposition(Vector vector1, Vector vector2) {
+        int minN = Math.min(vector1.n, vector2.n);
+        int maxN = Math.max(vector1.n, vector2.n);
+
+        Vector vector3 = new Vector(maxN);
+
+        for (int i = 0; i < minN; i++) {
+            vector3.array[i] = vector1.array[i] * vector2.array[i];
+        }
+        Arrays.fill(vector3.array, minN, maxN, 0.0);
+
+        return vector3;
     }
 }
