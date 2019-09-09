048047b491f442a9196429878abc37ebd8590383
Lavstor <45934398+Lavstor@users.noreply.github.com>
2019-09-09 13:43:34 +0300

    Vector/Matrix исправил

--- ArrayListHome/src/ru/academitschool/arraylisthome/main/Main.java
+++ ArrayListHome/src/ru/academitschool/arraylisthome/main/Main.java
@@ -1,12 +1,30 @@
 package ru.academitschool.arraylisthome.main;
 
-import ru.academitschool.arraylisthome.arraylist.ArrayListHome;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        ArrayListHome arrayList = new ArrayListHome(args[0]);
+        ArrayList<Integer> names = readFile(args[0]);
+    }
+    private static ArrayList<Integer> readFile(String path){
+        ArrayList<Integer> names = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(path))) {
 
-        System.out.println(arrayList);
-        System.out.println(arrayList.sortArrayList());
+            while (scanner.hasNextInt()) {
+                Integer number = scanner.nextInt();
+
+                if(number % 2 != 0){
+                    names.add(number);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return names;
     }
 }
