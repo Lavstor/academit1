0a66e741582c8aa16be11f53d45ec3fabbe9fb69
Lavstor <45934398+Lavstor@users.noreply.github.com>
2019-11-09 09:07:28 +0300

    Лекция 18 задачи Serialization

--- Serialization/src/ru/academit/school/serialization/main/Main.java
+++ Serialization/src/ru/academit/school/serialization/main/Main.java
@@ -2,11 +2,22 @@ package ru.academit.school.serialization.main;
 
 import ru.academit.school.serialization.matrix.Matrix;
 
-public class Main {
-    public static void main(String[] args) {
-        int[][] array = {{1, 2, 3}, {2, 4, 1}, {3, 1, 4}};
-        Matrix matrix1 = new Matrix(array);
+import java.io.*;
 
-        System.out.println(matrix1);
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
+        //int[][] array = {{1, 2, 3}, {2, 1, 4}, {3, 4, 1}};
+        int[][] array = {{1, 0, 9, 6}, {0, 1, 6, 7}, {9, 6, 1, 5}, {6, 7, 5, 1}};
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(args[1]))) {
+            out.writeObject(new Matrix(array));
+        }
+
+        try (ObjectInputStream in = new ObjectInputStream(
+                new FileInputStream(args[1]))) {
+            Matrix matrix1 = (Matrix) in.readObject();
+
+            System.out.println(matrix1);
+        }
     }
 }
--- Serialization/src/ru/academit/school/serialization/matrix/Matrix.java
+++ Serialization/src/ru/academit/school/serialization/matrix/Matrix.java
@@ -1,9 +1,14 @@
 package ru.academit.school.serialization.matrix;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Arrays;
 
-public class Matrix {
+public class Matrix implements Serializable {
     private int[][] matrix;
+    static final long serialVersionUID = 1L;
 
     public Matrix(int[][] array) {
         for (int i = 0; i < array.length; i++) {
@@ -34,5 +39,43 @@ public class Matrix {
         }
 
         return table.toString();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeInt(matrix.length);
+
+        for (int i = 0; i < matrix.length; i++) {
+            out.writeInt(matrix[0][i]);
+        }
+
+        out.writeInt(matrix[matrix.length - 1][matrix.length - 2]);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        int length = in.readInt();
+        matrix = new int[length][length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            int x = in.readInt();
+
+            matrix[0][i] = x;
+            matrix[i][0] = x;
+
+            if (i == 0) {
+                for (int j = 1; j < matrix.length; j++) {
+                    matrix[j][j] = x;
+                }
+            }
+
+            if (i == matrix.length - 1 && matrix.length % 2 == 0) {
+                for (int j = 1; j < matrix.length; j++) {
+                    matrix[j][matrix.length - j - 1] = x;
+                }
+            }
+        }
+        int x = in.readInt();
+
+        matrix[matrix.length - 1][matrix.length - 2] = x;
+        matrix[matrix.length - 2][matrix.length - 1] = x;
     }
 }
