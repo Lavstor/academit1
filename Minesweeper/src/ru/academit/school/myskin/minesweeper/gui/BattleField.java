e0170e8abb1b79314c4cd72de05aca7913f7cbfd
Lavstor <netynikneima@gmail.com>
2019-12-16 05:26:53 +0200

    Minesweeper 14/12/2019

--- Minesweeper/src/ru/academit/school/myskin/minesweeper/gui/BattleField.java
+++ Minesweeper/src/ru/academit/school/myskin/minesweeper/gui/BattleField.java
@@ -353,7 +353,6 @@ class BattleField extends JPanel {
                 add(gamePanel, BorderLayout.CENTER);
                 gamePanel.updateUI();
                 setScore();
-                System.out.println(player.getScore());
                 updatePlayer.doClick();
             }
         });
